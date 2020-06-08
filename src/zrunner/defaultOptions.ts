f640cf05a38714e07bf5993f54de9e87f0892d8a
Vladislav Taranov <vladislav.taranov@live.com>
2020-06-08 14:21:53 +0300

    Default options added

---
+++ src/zrunner/defaultOptions.ts
@@ -0,0 +1,13 @@
+import constants from './zrunner/constants';
+
+export default {
+  type: constants.HORIZONTAL,
+  min: 0,
+  max: 100,
+  step: 1,
+  value: 50,
+  isRange: true,
+  startValue: 20,
+  endValue: 80,
+  areValuesVisible: true
+};
